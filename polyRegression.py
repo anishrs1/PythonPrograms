df5d8b9f2ddcb422f8026c44413e0456544ae8e4
anishrs1 <34525648+anishrs1@users.noreply.github.com>
2022-12-07 22:38:09 -0600

    Add files via upload

---
+++ polyRegression.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+plottedgraph = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+bestfitline = numpy.linspace(1, 22, 100)
+
+plt.scatter(x, y)
+plt.plot(bestfitline, plottedgraph(bestfitline))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
